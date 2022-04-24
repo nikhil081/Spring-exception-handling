6d9686f3bf31e31bdaf55f1600b7eba64282d155
nikhil081 <nikmakkad08@gmail.com>
2022-04-25 00:55:04 +0530

    handled exceptions

--- src/main/java/com/example/errorhandling/advice/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/example/errorhandling/advice/CustomResponseEntityExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,20 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 @RestController
-public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = {ConstraintViolationException.class})
-    public ResponseEntity<Object> handleConstraint(ConstraintViolationException ex,
-                                                   WebRequest request ) {
-        return new ResponseEntity("errorSSSS", HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleInvalidArgument(MethodArgumentNotValidException ex){
+        Map<String,String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+        });
+        return errorMap;
     }
 
-
-    @ExceptionHandler(value = {Exception.class})
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {;
-        return new ResponseEntity("ErrorDDD", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
